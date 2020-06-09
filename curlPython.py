0ac0ab20734f8fd9253bc2fbdc5b3fc2cbb114c6
Yuval <yuval.haornoga@automat-it.com>
2020-06-09 17:38:34 +0300

    fixed date

--- curlPython.py
+++ curlPython.py
@@ -16,7 +16,7 @@ def ec2_servicecode():
         print (region)
         region_json = urlopen("https://pricing.us-east-1.amazonaws.com" + regionsURL['regions'][region]['currentVersionUrl'])
         region_load = json.loads(region_json.read())
-        filename = region.replace("-","_") + "_AmazonEC2" + "_" + now.strftime("%Y-%m-%d") + ".json"
+        filename = region.replace("-","_") + "_AmazonEC2" + "_" + now.strftime("%Y_%m_%d") + ".json"
         with open("/tmp/" + filename, mode='w') as region_file:
             region_file.write(json.dumps(region_load,indent=1,sort_keys=True))
         s3.meta.client.upload_file( "/tmp/" + filename, "irelandec2-prices", region + "/" + "EC2/" + filename)
@@ -32,7 +32,7 @@ def rds_servicecode():
         print (region)
         region_json = urlopen("https://pricing.us-east-1.amazonaws.com" + regionsURL['regions'][region]['currentVersionUrl'])
         region_load = json.loads(region_json.read())
-        filename = region.replace("-","_") + "_AmazonRDS" + "_" + now.strftime("%Y-%m-%d") + ".json"
+        filename = region.replace("-","_") + "_AmazonRDS" + "_" + now.strftime("%Y_%m_%d") + ".json"
         with open("/tmp/" + filename, mode='w') as region_file:
             region_file.write(json.dumps(region_load,indent=1,sort_keys=True))
         s3.meta.client.upload_file( "/tmp/" + filename, "irelandec2-prices", region + "/" + "RDS/" + filename)
